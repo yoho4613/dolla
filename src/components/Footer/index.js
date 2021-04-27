111e5e70de99e81173265a5852266fb5e025306a
yoho4613 <79840170+yoho4613@users.noreply.github.com>
2021-04-27 23:53:14 +1200

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { animateScroll as scroll } from "react-scroll";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItem,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIconLink,
+  SocialIcons,
+} from "./FooterElements";
+
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItem>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItem>
+            <FooterLinkItem>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/signin">Submit Video</FooterLink>
+              <FooterLink to="/signin">Ambassadors</FooterLink>
+              <FooterLink to="/signin">Agency</FooterLink>
+              <FooterLink to="/signin">Influencer</FooterLink>
+            </FooterLinkItem>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItem>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/signin">Contact</FooterLink>
+              <FooterLink to="/signin">Support</FooterLink>
+              <FooterLink to="/signin">Destinations</FooterLink>
+              <FooterLink to="/signin">Sponsorships</FooterLink>
+            </FooterLinkItem>
+            <FooterLinkItem>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/signin">Instagram</FooterLink>
+              <FooterLink to="/signin">Facebook</FooterLink>
+              <FooterLink to="/signin">Youtube</FooterLink>
+              <FooterLink to="/signin">Twitter</FooterLink>
+            </FooterLinkItem>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/" onClick={toggleHome}>
+              dolla
+            </SocialLogo>
+            <WebsiteRights>
+              dolla © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink
+                href="//www.facebook.com/"
+                target="_blank"
+                aria-label="Facebook"
+              >
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  discription:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg").default,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "UnLimited Access",
+  headline: "Login to your account at any time",
+  discription:
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.",
+  buttonLabel: "Learn More",
+  imgStart: false,
+  img: require("../../images/svg-2.svg").default,
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headline: "Creating an account is extremely easy",
+  discription:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start now",
+  imgStart: false,
+  img: require("../../images/svg-3.svg").default,
+  alt: "Paper",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
